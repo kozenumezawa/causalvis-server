f0f13bc9449e8bd226df2be778e88c035baea7ec
kozenumezawa <umezawa@viz.media.kyoto-u.ac.jp>
2017-10-15 21:51:12 +0900

    Update: response header

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from clusteringapi import Clustering
 class CORSMiddleware:
     def process_request(self, req, resp):
         resp.set_header('Access-Control-Allow-Origin', '*')
-        # resp.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+        resp.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
 
 api = falcon.API(middleware=[CORSMiddleware()])
 
